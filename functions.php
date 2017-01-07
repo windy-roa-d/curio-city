0533589f05fa0dbe913c3fadcbf135994d629b44
windy-roa-d <feelingskyblue.journey@gmail.com>
2017-01-08 02:22:35 +0900

    キャプション付き画像の表示調整を追加

--- functions.php
+++ functions.php
@@ -9,3 +9,32 @@ function include_parent_skins(){
 add_editor_style();
 
 //以下にSimplicity子テーマ用の関数を書く
+//キャプション指定時、Wordpressのデフォルトで横幅が+10pxされてしまうため関数で上書きする
+add_shortcode('caption', 'my_img_caption_shortcode');
+
+function my_img_caption_shortcode($attr, $content = null) {
+	if ( ! isset( $attr['caption'] ) ) {
+		if ( preg_match( '#((?:<a [^>]+>s*)?<img [^>]+>(?:s*</a>)?)(.*)#is', $content, $matches ) ) {
+			$content = $matches[1];
+			$attr['caption'] = trim( $matches[2] );
+		}
+	}
+
+	$output = apply_filters('img_caption_shortcode', '', $attr, $content);
+	if ( $output != '' )
+		return $output;
+
+	extract(shortcode_atts(array(
+		'id'	=> '',
+		'align'	=> 'alignnone',
+		'width'	=> '',
+		'caption' => ''
+	), $attr, 'caption'));
+
+	if ( 1 > (int) $width || empty($caption) )
+		return $content;
+
+	if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+
+	return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" style="width: ' . $width . 'px">' . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
